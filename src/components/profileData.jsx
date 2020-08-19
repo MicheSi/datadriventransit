762137ee056592592009bdf50738b831ba753883
mlazzell17@gmail.com <MicheSi>
2020-08-19 10:24:21 -0700

    initial commit

---
+++ src/components/profileData.jsx
@@ -0,0 +1,95 @@
+import agustin from '../images/agustin.jpg';
+import cody from '../images/cody.jpg';
+import connor from '../images/connor.jpg';
+import erik from '../images/erik.jpg';
+import jonathan from '../images/jonathan.jpg';
+import jordan from '../images/jordan.jpg';
+import justin from '../images/justin.jpg';
+import mathias from '../images/mathias.jpg';
+import michelle from '../images/michelle.jpg';
+
+const profileData = [
+    {
+        id: 1,
+        name: 'Agustin Vargas',
+        title: 'Data Scientist',
+        bio: 'An aspiring data scientist, with a background in scientific research and higher education administration; intuitive by nature and optimistic by habit.  My role in this project was to work with the DS team to gain insight into the traffic patterns of vehicles within the San Francisco public transit system.',
+        img: agustin,
+        github: 'https://github.com/avdata',
+        linkedIn: 'http://linkedin.com/in/vargasstem'
+    },
+    {
+        id: 2,
+        name: 'Cody Holman',
+        title: 'Web Developer',
+        bio: 'Bio will go here.',
+        img: cody,
+        github: 'https://github.com/cholman',
+        linkedIn: 'https://www.linkedin.com/in/cody-holman'
+    },
+    {
+        id: 3,
+        name: 'Connor Angelis',
+        title: 'Web Developer',
+        bio: 'A growing front-end developer, with a short lifetime of always working with computers and technology. A nerd at heart and constantly working to dig myself a home in the software industry. My role in this project was working with the Plotly React API to bring a visualization of the San Francisco metro system to life.',
+        img: connor,
+        github: 'https://github.com/KonstadinosAngelis',
+        linkedIn: 'https://www.linkedin.com/in/konstadinos-a-a8a641199/'
+    },
+    {
+        id: 4,
+        name: 'Erik Sandoval',
+        title: 'Web Developer & Team Lead',
+        bio: 'Nintendo and Hardware enthusiast, specializes in making and fixing own mistakes.',
+        img: erik,
+        github: 'https://github.com/erik-sandoval',
+        linkedIn: ''
+    },
+    {
+        id: 5,
+        name: 'Jonathan Allison',
+        title: 'Web Developer',
+        bio: 'Jonathan is a Web Developer from North Carolina and proud husband, father, and grandfather. Not afraid to still chase my dreams.',
+        img: jonathan,
+        github: 'https://github.com/qb1968',
+        linkedIn: 'https://www.linkedin.com/in/jonathan-allison-1a4136196/'
+    },
+    {
+        id: 6,
+        name: 'Jordan Ireland',
+        title: 'Data Scientist',
+        bio: 'Data Scientist by day and game developer by night. I have been programming for over 10 years and have a background in medicine and optics. I worked on creating the API that would be called to get the front end information and to support the rest of the Data Science team.',
+        img: jordan,
+        github: 'https://github.com/Jordan-Ireland',
+        linkedIn: 'https://www.linkedin.com/in/jordan-b-ireland/'
+    },
+    {
+        id: 7,
+        name: 'Justin Menendez',
+        title: 'UX Designer',
+        bio: 'Like if Andre 3000 were a bohemian mystic.  Helped with Design and Creative Thinking.  Do the Good.',
+        img: justin,
+        github: '',
+        linkedIn: 'https://www.linkedin.com/in/justinmenendez/'
+    },
+    {
+        id: 8,
+        name: 'Mathias Skreden',
+        title: 'Data Scientist',
+        bio: 'Bio will go here.',
+        img: mathias,
+        github: 'https://github.com/skredenmathias/',
+        linkedIn: 'https://www.linkedin.com/in/skredenmathias/'
+    },
+    {
+        id: 9,
+        name: 'Michelle Sirimanivong',
+        title: 'Web Developer',
+        bio: 'Michelle is a full-stack web developer from Arizona. In her free time, she enjoys baking, playing video games and spending time with her husband and her 3 dogs.',
+        img: michelle,
+        github: 'https://github.com/MicheSi',
+        linkedIn: 'https://www.linkedin.com/in/michellesirimanivong/'
+    }
+]
+
+export default profileData;
